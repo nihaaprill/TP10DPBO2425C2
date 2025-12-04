851507b78316dd3ea599b4b1266c329ccc10a750
nihaaa <nihaaapril@gmail.com>
2025-12-04 21:55:01 +0700

    Add files via upload

---
+++ ProjectMVVM/Config/Database.php
@@ -0,0 +1,29 @@
+<?php
+
+class Database {
+    private $host = "localhost";
+    private $dbname = "bioskop_db";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function connect() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->dbname,
+                $this->username,
+                $this->password
+            );
+            // Setting biar error PDO muncul (debug enak)
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        } catch (PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
+?>
